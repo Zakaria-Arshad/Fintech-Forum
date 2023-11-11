a80827bc5f5ad82deb713f642d22a047cfd683f0
Zakaria Arshad <zakaria.arshad@nyu.edu>
2023-11-11 16:29:20 -0500

    testing

--- src/supabaseClient.jsx
+++ src/supabaseClient.jsx
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://qrpctkcqotdmshdbdiub.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFycGN0a2Nxb3RkbXNoZGJkaXViIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyMDkyMjcsImV4cCI6MjAxNDc4NTIyN30.qT0-2v266YyMyrUU9yKiROUveTNTgPrNqMdhAk04AYA';
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
