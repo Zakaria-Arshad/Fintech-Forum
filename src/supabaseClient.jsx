6d402a464ff88caf8473fd4a0895f9bd8dd3f029
Zakaria Arshad <zakaria.arshad@nyu.edu>
2023-11-11 16:34:38 -0500

    update client

--- src/supabaseClient.jsx
+++ src/supabaseClient.jsx
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://qrpctkcqotdmshdbdiub.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFycGN0a2Nxb3RkbXNoZGJkaXViIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyMDkyMjcsImV4cCI6MjAxNDc4NTIyN30.qT0-2v266YyMyrUU9yKiROUveTNTgPrNqMdhAk04AYA';
+const supabaseUrl = 'https://qrpctkcqotdmshdbdiub.supabase.co'; // import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFycGN0a2Nxb3RkbXNoZGJkaXViIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkyMDkyMjcsImV4cCI6MjAxNDc4NTIyN30.qT0-2v266YyMyrUU9yKiROUveTNTgPrNqMdhAk04AYA'; // import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
